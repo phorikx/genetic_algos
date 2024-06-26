9d46d232857c7a1f7ae913bd0c09168aa15e6dd9
Paul Horikx <p.horikx@gmail.com>
2024-06-26 13:03:19 +0200

    start portfolio

---
+++ genetic/scripts/portfolio.exs
@@ -0,0 +1,25 @@
+defmodule Portfolio do
+  @behaviour Problem
+  alias Types.Chromosome
+
+  @target_fitness 180
+
+  @impl true
+  def genotype() do
+    genes = 
+      for _ <- 1..10 do
+        {:rand.uniform(10), :rand.uniform(10)}
+      end
+    %Chromosome{genes: genes, size: 10}
+  end
+
+  @impl true
+  def fitness_function(chromosome) do
+    # TODO
+  end
+
+  def terminate?(population, _generation) do
+    max_value = Enum.max_by(population, &Portfolio.fitness_function/1)
+    max_value > @target_fitness
+  end
+end
