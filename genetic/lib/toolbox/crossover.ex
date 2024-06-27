37a60de1ca4b33fac311a726c7f494b7d27b2f2e
Paul Horikx <p.horikx@gmail.com>
2024-06-27 22:32:34 +0200

    added some crossover strategies - ch6

---
+++ genetic/lib/toolbox/crossover.ex
@@ -0,0 +1,49 @@
+defmodule Toolbox.Selection do
+  alias Types.Chromosome
+
+  def order_one_crossover(p1, p2) do
+    lim = Enum.count(p1.genes) - 1
+
+    {i1, i2} =
+      [:rand.uniform(lim), :rand.uniform(lim)]
+      |> Enum.sort()
+      |> List.to_tuple()
+
+    slice1 = Enum.slice(p1.genes, i1..i2)
+    slice1_set = MapSet.new(slice1)
+    p2_contrib = Enum.reject(p2.genes, &MapSet.member?(slice1_set, &1))
+    {head1, tail1} = Enum.split(p2_contrib, i1)
+
+    slice2 = Enum.slice(p2.genes, i1..i2)
+    slice2_set = MapSet.new(slice2)
+    p1_contrib = Enum.reject(p1.genes, &MapSet.member?(slice2_set, &1))
+    {head2, tail2} = Enum.split(p1_contrib, i1)
+
+    {c1, c2} = {head1 ++ slice1 ++ tail1, head2 ++ slice2 ++ tail2}
+    {%Chromosome{genes: c1, size: p1.size}, %Chromosome{genes: c2, size: p1.size}}
+  end
+
+  def single_point_crossover(p1, p2) do
+    cx_point = :rand.uniform(p1.size)
+    {p1_head, p1_tail} = Enum.split(p1.genes, cx_point)
+    {p2_head, p2_tail} = Enum.split(p2.genes, cx_point)
+    {c1, c2} = {p1_head ++ p2_tail, p2_head ++ p1_tail}
+    {%Chromosome{genes: c1, size: p1.size}, {%Chromosome{genes: c2, size: p1.size}}}
+  end
+
+  def uniform_crossover(p1, p2, rate \\ 0.5) do
+    {c1, c2} =
+      p1.genes
+      |> Enum.zip(p2.genes)
+      |> Enum.map(fn {x, y} ->
+        if :rand.uniform() < rate do
+          {x, y}
+        else
+          {y, x}
+        end
+      end)
+      |> Enum.unzip()
+
+    {%Chromosome{genes: c1, size: length(c1)}, %Chromosome{genes: c2, size: length(c2)}}
+  end
+end
